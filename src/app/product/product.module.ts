db52829eabf422c239668628fe7e018875e237da
harauma <fot39hara@gmail.com>
2021-07-11 16:38:31 +0900

    productページ ルーディング修正

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,17 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { ProductListComponent } from './product-list/product-list.component';
 import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductComponent } from './product.component';
 
 const routes: Routes = [
-  { path: '', component: ProductListComponent },
-  { path: 'detail', component: ProductDetailComponent },
+  { path: 'products', component: ProductComponent,
+    children: [
+      { path: '', component: ProductListComponent },
+      { path: 'detail', component: ProductDetailComponent },
+    ]
+  },
 ];
 
 @NgModule({
   declarations: [
+    ProductComponent,
     ProductDetailComponent,
     ProductListComponent
   ],
